5b61ffa68c07c69e607f07073cd0bd9864b56080
Christophe Hébert <christophe@imagescreations.fr>
2018-12-19 14:51:23 +0100

    Add auth/signin component

--- bookshelves/src/app/app.module.ts
+++ bookshelves/src/app/app.module.ts
@@ -3,11 +3,13 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { SignupComponent } from './auth/signup/signup.component';
+import { SigninComponent } from './auth/signin/signin.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    SignupComponent
+    SignupComponent,
+    SigninComponent
   ],
   imports: [
     BrowserModule
