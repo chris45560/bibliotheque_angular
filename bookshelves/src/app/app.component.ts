0b68f11a42cfbd67506d423340b1398ab3f1e233
Christophe Hébert <christophe@imagescreations.fr>
2018-12-21 13:54:50 +0100

    Add authentification system with firebase

--- bookshelves/src/app/app.component.ts
+++ bookshelves/src/app/app.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+import * as firebase from 'firebase';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'bookshelves';
+    title = 'bookshelves';
+
+    constructor() {
+        const config = {
+            apiKey: 'AIzaSyBgnN0QIxQj-ZlTrYtpobnRktT7kHW9eEI',
+            authDomain: 'bookshelves-de362.firebaseapp.com',
+            databaseURL: 'https://bookshelves-de362.firebaseio.com',
+            projectId: 'bookshelves-de362',
+            storageBucket: 'bookshelves-de362.appspot.com',
+            messagingSenderId: '542640883613'
+        };
+        firebase.initializeApp(config);
+    }
 }
